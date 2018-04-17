3b3fed87ae1acdfe06896dcfc69065d493ea3b82
Jasmin Lehtinen <jasminle@metropolia.fi>
2018-04-17 11:04:45 +0300

    Added unit tests

---
+++ src/test/java/org/gradle/demo/GreeterTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gradle.demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author jasmin
+ */
+public class GreeterTest {
+    
+    @Test
+    public void dummyUnitTest() {
+        assertEquals(0, 1);
+    }
+    
+    @Test
+    public void dummyUnitTest2() {
+        assertEquals(1, 1);
+    }
+}
